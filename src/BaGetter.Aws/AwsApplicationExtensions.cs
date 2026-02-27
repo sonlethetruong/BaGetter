e60c1f2047dc5d7c6287879edce8dd07f49096fc
sonlethetruong <53509390+sonlethetruong@users.noreply.github.com>
2026-02-28 00:31:07 +0700

    Add ForcePathStyle to AmazonS3Config initialization
    Signed-off-by: sonlethetruong <53509390+sonlethetruong@users.noreply.github.com>

--- src/BaGetter.Aws/AwsApplicationExtensions.cs
+++ src/BaGetter.Aws/AwsApplicationExtensions.cs
@@ -32,7 +32,7 @@ public static class AwsApplicationExtensions
             var options = provider.GetRequiredService<IOptions<S3StorageOptions>>().Value;
 
             var config = options.Endpoint != null
-                ? new AmazonS3Config { ServiceURL = options.Endpoint.AbsoluteUri }
+                ? new AmazonS3Config { ServiceURL = options.Endpoint.AbsoluteUri, ForcePathStyle = options.ForcePathStyle }
                 : new AmazonS3Config { RegionEndpoint = RegionEndpoint.GetBySystemName(options.Region) };
 
             if (options.UseInstanceProfile)
